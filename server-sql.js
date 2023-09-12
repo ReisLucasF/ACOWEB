3d37b32a33e07d01a8d0eae72aa007cf08a41e19
ReislucasF <reislucasf@icloud.com>
2023-09-12 15:49:24 -0300

    adição de mongodb

--- server-sql.js
+++ server-sql.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectID } = require('mongodb');
+const mysql = require('mysql2/promise');
 require('dotenv').config();
 const path = require('path');
 
@@ -9,23 +8,12 @@ const app = express();
 app.use(cors());
 const port = process.env.PORT || 3000;
 
-const client = new MongoClient(process.env.MONGODB_URI, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-  tls: true,
-  tlsInsecure: true,
-});
-
 let db;
 
 async function run() {
   try {
-    await client.connect();
-    db = client.db('acowebb');
-    console.log("Conexão bem-sucedida ao MongoDB");
+    db = await mysql.createConnection(process.env.DATABASE_URL);
+    console.log("Conexão bem-sucedida ao MySQL");
     app.listen(port, () => {
       console.log(`Servidor rodando em http://localhost:${port}`);
     });
@@ -37,7 +25,6 @@ async function run() {
 run();
 
 app.use(express.static('public'));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
@@ -48,26 +35,25 @@ app.get('/adm.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'adm.html'));
 });
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 // Rota para obter todos os redirecionamentos
 app.get('/api/redirecionamentos', async (req, res) => {
   try {
-    const collection = db.collection('redirecionamentos');
-    const data = await collection.find().toArray();
-    res.json(data);
+    const [rows] = await db.execute('SELECT * FROM redirecionamentos');
+    res.json(rows);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 });
 
-// Rota para adicionar um novo redirecionamento
 app.post('/api/redirecionamentos', async (req, res) => {
   try {
-    const collection = db.collection('redirecionamentos');
-    const result = await collection.insertOne(req.body);
-    res.json({ id: result.insertedId, ...req.body });
+    const { nome, link, codigo } = req.body;
+    const [result] = await db.execute('INSERT INTO redirecionamentos (nome, link, codigo) VALUES (?, ?, ?)', [nome, link, codigo]);
+    res.json({ id: result.insertId, nome, link, codigo });
   } catch (err) {
+    console.error('Erro ao inserir redirecionamento:', err);
     res.status(500).json({ error: err.message });
   }
 });
@@ -75,15 +61,14 @@ app.post('/api/redirecionamentos', async (req, res) => {
 // Rota para deletar um redirecionamento
 app.delete('/api/redirecionamentos/:id', async (req, res) => {
   try {
-    const collection = db.collection('redirecionamentos');
-    const result = await collection.deleteOne({ _id: new ObjectID(req.params.id) });
-    console.log("Resultado da deleção:", result);  // Log do resultado no console do servidor
-    if (result.deletedCount === 0) {
+    const [result] = await db.execute('DELETE FROM redirecionamentos WHERE id = ?', [req.params.id]);
+    console.log("Resultado da deleção:", result); 
+    if (result.affectedRows === 0) {
       return res.status(404).json({ message: 'Nenhum redirecionamento encontrado com esse ID' });
     }
     res.json({ message: 'Redirecionamento deletado' });
   } catch (err) {
-    console.error(err);  // Log do erro no console do servidor
+    console.error(err);
     res.status(500).json({ error: err.message });
   }
 });
