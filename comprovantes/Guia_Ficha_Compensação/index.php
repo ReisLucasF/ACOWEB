b9dc4a8be68adbd4901cafc44182447a9d6e2852
Lucas Reis <113260706+ReisLucasF@users.noreply.github.com>
2024-01-16 23:09:56 +0000

    Feat: Titulos comprovantes

--- comprovantes/Guia_Ficha_Compensação/index.php
+++ comprovantes/Guia_Ficha_Compensação/index.php
@@ -45,7 +45,8 @@
 
                     <!-- Page TItulo -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <!--<h1 class="h3 mb-0 text-gray-800">Dashboard</h1>-->
+                        <h1 class="h3 mb-0 text-gray-800 mt-5">Gerar comprovante - Ficha Compensação</h1>
+
                     </div>
 
                     <!-- Content Row -->
