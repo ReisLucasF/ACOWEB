7a7544ab14bf015a4c811f17951c32be43ad98de
Lucas Reis <113260706+ReisLucasF@users.noreply.github.com>
2024-01-16 23:22:20 +0000

    Footer

--- sections/footer.php
+++ sections/footer.php
@@ -1,7 +1,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Copyright &copy; ACO_SQC 2024</span>
+            <span>Copyright &copy; TB Utilities 2024</span>
         </div>
     </div>
 </footer>
