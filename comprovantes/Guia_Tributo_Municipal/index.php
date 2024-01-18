bbf18258d48701692467a6056f4037ba2cac08cf
Lucas Reis <113260706+ReisLucasF@users.noreply.github.com>
2024-01-18 18:18:17 +0000

    remove cache

--- comprovantes/Guia_Tributo_Municipal/index.php
+++ comprovantes/Guia_Tributo_Municipal/index.php
@@ -1,3 +1,8 @@
+<?php
+header("Cache-Control: no-cache, must-revalidate");
+header("Pragma: no-cache");
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+header("Cache-Control: no-cache, must-revalidate");
+header("Pragma: no-cache");
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
