c640d874c84f29300820563d146872a073fb1306
Lucas Reis <113260706+ReisLucasF@users.noreply.github.com>
2024-01-24 00:34:53 +0000

    finish

--- aco/script.js
+++ aco/script.js
@@ -20,16 +20,20 @@
     }
 
     function update_preview(){
+        // layout1
+        // lógica que lê a imagem64 para png        
         var base64String = "data:image/png;base64," + imagemcode;
-
         document.getElementById('cardPreviewIMG').style.backgroundImage = `url(${base64String})`;
 
+        //   Estilização do titulo
         document.getElementById('tituloPreview').textContent = json["Titulo"];
         document.getElementById('tituloPreview').style.color = json["Valor"]["ItemCard"]["ImagemFundo"]["CorTitulo"];
 
+        //   Estilização do subtitulo
         document.getElementById('subtituloPreview').textContent = json["Valor"]["ItemCard"]["Complemento"]["SubTitulo"];
         document.getElementById('subtituloPreview').style.color = json["Valor"]["ItemCard"]["ImagemFundo"]["CorSubTitulo"];
 
+        //   estilização do CTA
         document.getElementById('textoCTAPreview').textContent = json["Valor"]["ItemCard"]["Complemento"]["TextoCta"];
         document.getElementById('textoCTAPreview').style.color = json["Valor"]["ItemCard"]["ImagemFundo"]["CorTextoCta"];
         document.getElementById('textoCTAPreview').style.backgroundColor = json["Valor"]["ItemCard"]["ImagemFundo"]["CorFundoCta"];
@@ -43,6 +47,7 @@
         fr.onload = function() {
             content_textarea.textContent = this.result;
 
+            //span mostrando o arquivo
             if (txt_file.files.length > 0) {
                 statusArquivo.textContent = `Arquivo selecionado: ${txt_file.files[0].name}`;
             } else {
@@ -66,3 +71,16 @@
             alert("Script não selecionado");
         }
     });
+
+    //// VARIÁVEIS DO SCRIPT PARA MONTAR O LAYOUT ////
+    // let tipo_de_layout = json["Valor"]["ItemCard"]["IdTipoRecurso"]
+    // let titulo = json["Titulo"]
+    // let titulo_cor = json["Valor"]["ItemCard"]["ImagemFundo"]["CorTitulo"]
+    // let subtitulo = json["Valor"]["ItemCard"]["Complemento"]["SubTitulo"]
+    // let subtitulo_cor = json["Valor"]["ItemCard"]["ImagemFundo"]["CorSubTitulo"]
+    // let texto_cta = json["Valor"]["ItemCard"]["Complemento"]["TextoCta"]
+    // let texto_cta_cor = json["Valor"]["ItemCard"]["ImagemFundo"]["CorTextoCta"]
+    // let cor_fundo_inicial = json["Valor"]["ItemCard"]["ImagemFundo"]["CorInicio"]
+    // let cor_fundo_final = json["Valor"]["ItemCard"]["ImagemFundo"]["CorFim"]
+    // let cor_fundo_cta = json["Valor"]["ItemCard"]["ImagemFundo"]["CCorFundoCta"]
+    // let cor_borda_cta = json["Valor"]["ItemCard"]["ImagemFundo"]["CorBordaCta"]
