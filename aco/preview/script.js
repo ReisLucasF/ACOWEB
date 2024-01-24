f8a840aebc1129de6c57cd25f3dbb65c85f66c04
Lucas Reis <113260706+ReisLucasF@users.noreply.github.com>
2024-01-24 02:08:52 +0000

    a

--- aco/preview/script.js
+++ aco/preview/script.js
@@ -29,6 +29,8 @@
     // Dividindo os valores por vírgula e removendo espaços em branco de cada valor
     let valores = valoresStr.split(',').map(valor => valor.trim());
     let layout = valores[2];
+    console.log(layout)
+    console.log(typeof(layout))
 
     att_preview()
     }
