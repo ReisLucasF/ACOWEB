ca117ca97eb80549fbb0be99009528365e1ce41a
Lucas Reis <113260706+ReisLucasF@users.noreply.github.com>
2024-06-21 13:46:19 +0000

    criação comprovante FGTS layout diversos

---
+++ comprovantes/Guia_FGTS/script.js
@@ -0,0 +1,155 @@
+async function generatePDF() {
+  const textInput = document.getElementById('textInput').value;
+  const Numbers_zeros = [0,0,0,0,0,0,0,0,0,0];
+  // Extrair os dados do texto
+  const valorDocumentoMatch = textInput.match(/Valor do documento\s*:\s*R\$\s*([\d,.]+)/i);
+  const valorPagoMatch = textInput.match(/Valor liquido a debitar\s*:\s*R\$\s*([\d,.]+)/i);
+  const valorDescontoMatch = textInput.match(/Valor do desconto\s*:\s*R\$\s*([\d,.]+)/i);
+  const valorEncargosMatch = textInput.match(/Valor dos juros\/multa\s*:\s*R\$\s*([\d,.]+)/i);
+  const codigoBarrasMatch = textInput.match(/Codigo de Barras\s*:\s*(\d{44})/i);
+  const dataMovimentoMatch = textInput.match(/Data do movimento\s*:\s*(\d{2})\/(\d{2})\/(\d{4})/i);
+  const nsuMatch = textInput.match(/Nsu\s*:\s*(\d+)/i);
+  
+  const agenciaRecebedoraMatch = textInput.match(/Agencia recebedora\s*:\s*(\d+)/i);
+  const horarioCanalMatch = textInput.match(/Hora no Canal\s*:\s*(\d{2}:\d{2}:\d{2})/i);
+  const dataVencimentoMatch = textInput.match(/Data de vencimento\s*:\s*(\d{2})\/(\d{2})\/(\d{4})/i);
+  const NomeMatch = textInput.match(/Nome do cliente\s*:\s*(.+)/i);
+  const agenciaMatch = textInput.match(/Agencia\s*:\s*(\d+)\s*-\s*([^\n]+)/i);
+  const formaPagamentoMatch = textInput.match(/Forma de Recebimento\s*:\s*(\d+)\s*-\s*([^\n]+)\b/i);
+  
+  const agenciaDescricao = agenciaMatch ? agenciaMatch[2] : 'N/A';
+  const formaPagamentoDescricao = formaPagamentoMatch ? formaPagamentoMatch[2] : 'N/A';
+ 
+  // Codigo do Convenio : 12173
+
+  const valorDocumento = valorDocumentoMatch && valorDocumentoMatch[1] ? parseFloat(valorDocumentoMatch[1].replace(',', '.')) : 0;
+  const valorPago = valorPagoMatch && valorPagoMatch[1] ? parseFloat(valorPagoMatch[1].replace(',', '.')) : 0;
+  const valorDocumentoFormatado = valorDocumento.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+  const valorPagoFormatado = valorPago.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+  const codigoBarras = codigoBarrasMatch ? codigoBarrasMatch[1] : '';
+  
+  const diaPagamento = dataMovimentoMatch ? dataMovimentoMatch[1] : '';
+  const mesPagamento = dataMovimentoMatch ? dataMovimentoMatch[2] : '';
+  const anoPagamento = dataMovimentoMatch ? dataMovimentoMatch[3].slice(-4) : '';
+  const nsu = nsuMatch ? nsuMatch[1].substr(-6) : '';
+  const agenciaRecebedora = agenciaRecebedoraMatch ? agenciaRecebedoraMatch[1] : '';
+  const horarioCanal = horarioCanalMatch ? horarioCanalMatch[1].substr(0, 5) : '';
+
+
+  const diaVencimento = dataVencimentoMatch ? dataVencimentoMatch[1] : '';
+  const mesVencimento = dataVencimentoMatch ? dataVencimentoMatch[2] : '';
+  const anoVencimento = dataVencimentoMatch ? dataVencimentoMatch[3] : '';
+
+  const dataVencimento = `${diaVencimento}/${mesVencimento}/${anoVencimento}`;
+
+  const convenioMatch = textInput.match(/Codigo do Convenio\s*:\s*(\d+)/i);
+  const convenio = convenioMatch ? convenioMatch[1] : '';
+
+  let convenioformatado = '';
+
+  const conveniosValidos = ["12173", "12157", "12190", "1221", "12513"];
+
+  if (conveniosValidos.includes(convenio)) {
+      convenioformatado = "0180 - com centralização ou com tomador";
+  } else if (convenio === "00116") {
+      convenioformatado = "418 - FGTS / GFIP";
+  } else if (convenio === "13617") {
+      convenioformatado = "411 - FGTS / GFIP";
+  } else if (convenio === "14010") {
+      convenioformatado = "412 - FGTS / GFIP";
+  } else {
+      // ...
+  }
+
+  console.log(convenioformatado);
+
+  let dataCompetenciaFormatada = ''
+
+  const competenciaMatch = textInput.match(/Data de competencia\s*:\s*(\d{2})(\d{4})/i);
+  if (competenciaMatch) {
+      const mes = competenciaMatch[1];
+      const ano = competenciaMatch[2];
+      dataCompetenciaFormatada = `${mes}/${ano}`;
+      console.log(dataCompetenciaFormatada); // Saída: 05/2018
+  } else {
+      console.log("Data de competência não encontrada.");
+  }
+
+
+  let valorJurosMulta = ''
+  const jurosMultaMatch = textInput.match(/Valor dos juros\/multa\s*:\s*R\$\s*([\d,\.]+)/i);
+  if (jurosMultaMatch) {
+      valorJurosMulta = jurosMultaMatch[1];
+      console.log(`Valor dos juros/multa: R$ ${valorJurosMulta}`); // Saída: Valor dos juros/multa: R$ 0,00
+  } else {
+      console.log("Valor dos juros/multa não encontrado.");
+  }
+  
+  //Realiza os 0 amais do valor na autenticação
+  const valordocNSU = valorDocumentoMatch[1].replace(',', '').split("");
+  var aux = Numbers_zeros.length;
+  for (let index = valordocNSU.length; index >= 0; index--) {
+    Numbers_zeros[aux] = valordocNSU[index];
+    aux--;
+  }
+  Numbers_zeros.pop()
+  //console.log(Numbers_zeros);
+  
+
+  // Remover caracteres especiais (":") do horário
+  const horarioCanalSemCaracteresEspeciais = horarioCanal.replace(/:/g, '');
+
+  // Calculando a autenticação conforme a fórmula fornecida
+  const autenticacao = `${agenciaRecebedora}${anoPagamento}${mesPagamento}${diaPagamento}${horarioCanalSemCaracteresEspeciais}${Numbers_zeros.join("")}${nsuMatch[1]}`;
+
+// Obter a data e hora atual no formato DD/MM/AAAA HH:mm
+const today = new Date();
+const dd = String(today.getDate()).padStart(2, '0');
+const mm = String(today.getMonth() + 1).padStart(2, '0'); // Janeiro é 0!
+const yyyy = today.getFullYear();
+
+const hours = String(today.getHours()).padStart(2, '0');
+const minutes = String(today.getMinutes()).padStart(2, '0');
+
+
+const dataEmissao = `${dd}/${mm}/${yyyy} ${hours}:${minutes}`;
+
+// Carregar o conteúdo do HTML externo
+const response = await fetch('table_template.html');
+const htmlContent = await response.text();
+
+
+  // Preencher os dados na tabela no HTML
+  const modifiedHtmlContent = htmlContent
+  .replace('<td id="codigoBarras"></td>', `<td class="foco" id="codigoBarras">${codigoBarras}</td>`)
+  .replace('<td id="competencia"></td>', `<td class="foco" id="competencia">${dataCompetenciaFormatada}</td>`)
+  .replace('<td id="valorDocumento"></td>', `<td class="foco" id="valorDocumento">${valorDocumentoFormatado}</td>`)
+  .replace('<td id="jurosMulta"></td>', `<td class="foco" id="jurosMulta">${valorJurosMulta}</td>`)
+  .replace('<td id="valorPago"></td>', `<td class="foco" id="valorPago">${valorPagoFormatado}</td>`)
+  .replace('<td id="desconto"></td>', `<td class="foco" id="desconto">R$ ${valorDescontoMatch[1]}</td>`)
+  .replace('<td id="encargos"></td>', `<td class="foco" id="encargos">R$ ${valorEncargosMatch[1]}</td>`)
+  .replace('<td id="canalPagamento"></td>', `<td class="foco" id="canalPagamento">${agenciaDescricao.replace('_', ' ')}</td>`)
+  .replace('<td id="formaPagamento"></td>', `<td class="foco" id="formaPagamento">${formaPagamentoDescricao}</td>`)
+  .replace('<td id="dataVencimento"></td>', `<td class="foco" id="dataVencimento">${dataVencimento}</td>`)
+  .replace('<td id="dataMovimento"></td>', `<td class="foco" id="dataMovimento">${diaPagamento}/${mesPagamento}/${anoPagamento}</td>`)
+  .replace('<td id="convenio"></td>', `<td class="foco" id="convenio">${convenioformatado}</td>`)
+  .replace('<td id="nsu"></td>', `<td class="foco" id="nsu">${nsuMatch[1]}</td>`)
+  .replace('<td id="nomepagador"></td>', `<td class="foco" id="nomepagador">${NomeMatch[1]}</td>`)
+  .replace('<td id="agenciaRecebedora"></td>', `<td class="foco" id="agenciaRecebedora">${agenciaRecebedora}</td>`)
+  .replace('<td id="autenticacao"></td>', `<td class="foco" id="autenticacao">0389${autenticacao}</td>`)
+  .replace('<td id="DataEmissão"></td>', `<td class="foco" id="DataEmissão">${dataEmissao}</td>`);
+
+
+
+  // Criar um elemento temporário para armazenar a tabela
+  const tempElement = document.createElement('div');
+  tempElement.innerHTML = modifiedHtmlContent;
+
+  // Converter a tabela HTML em PDF
+  html2pdf(tempElement, {
+    margin: [10, 10, 10, 10],
+    filename: 'comprovante_'+nsuMatch[1]+'.pdf',
+    html2canvas: { dpi: 600, scale: 4 },
+    jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+  });
+}
