753b159be6f13d093fc798610603a4c5f1e4019f
ReisLucasF <reislucasf@icloud.com>
2024-08-17 22:30:35 -0300

    retorno

--- get-api-key.php
+++ get-api-key.php
@@ -1,26 +1,4 @@
 <?php
-$allowed_domains = ['reislucasf.com.br', 'aco.reislucasf.com.br'];
-$referer = $_SERVER['HTTP_REFERER'] ?? '';
-
-$domain_valid = false;
-
-if ($referer) {
-    $referer_host = parse_url($referer, PHP_URL_HOST);
-
-    foreach ($allowed_domains as $domain) {
-        if (strpos($referer_host, $domain) !== false) {
-            $domain_valid = true;
-            break;
-        }
-    }
-}
-
-if (!$domain_valid) {
-    http_response_code(403);
-    echo json_encode(['error' => 'Acesso negado']);
-    exit;
-}
-
 require './vendor/autoload.php';
 
 use Dotenv\Dotenv;
